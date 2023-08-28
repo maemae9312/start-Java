ffc1918e1a5e0a70f8e423b3852252701b959a38
maemae9312 <maeda.hyo@kikitori.jp>
2023-08-28 22:30:57 +0900

    variable

---
+++ Variable.java
@@ -0,0 +1,8 @@
+public class Variable {
+    public static void main (String[] args) {
+        int month = 8;
+        int day = 28;
+
+        System.out.println("今日は、" + month + "月" + day + "日です！");
+    }
+}
