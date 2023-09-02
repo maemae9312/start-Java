3a432fe36a7a829b5a675a080195310f4b8d7988
maemae9312 <maeda.hyo@kikitori.jp>
2023-09-02 13:18:42 +0900

    Practice problem

---
+++ 2-6/Operator.java
@@ -0,0 +1,17 @@
+public class Operator {
+    public static void main(String[] args) {
+        // コマンドラインから入力したデータ(文字列)をint型に変換する
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+        System.out.println("a >= 0�F" + (a >= 0));
+        System.out.println("(a >= 0) && (b >= 0):" + ((a >= 0) && (b >= 0)));
+    }
+}
